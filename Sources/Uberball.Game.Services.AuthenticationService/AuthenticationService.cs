5dbdfa1d608bf7cffcbf6f3ce19eb4a675077b39
brainster <brainster-one@hotmail.com>
2012-03-10 15:15:52 +0700

    Authentication service.

---
+++ Sources/Uberball.Game.Services.AuthenticationService/AuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Uberball.Game.Services.AuthenticationService {
+	using System;
+
+	public class AuthenticationService : IAuthenticationService {
+		public Guid LogOn(string login, string password) {
+			return Guid.NewGuid();
+		}
+	}
+}
---
+++ Sources/Uberball.Game.Services.AuthenticationService/Contracts/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Uberball.Game.Services.AuthenticationService {
+	using System;
+	using System.ServiceModel;
+
+	[ServiceContract] public interface IAuthenticationService {
+		[OperationContract] 
+		Guid LogOn(string login, string password);
+	}
+}
