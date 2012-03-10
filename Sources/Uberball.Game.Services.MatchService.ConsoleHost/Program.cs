c702a17fa69917ab98f8f3e260be29746bc2e0a7
brainster <brainster-one@hotmail.com>
2012-03-10 14:53:57 +0700

    Basic MatchService.

--- Sources/Uberball.Game.Services.MatchService.ConsoleHost/Program.cs
+++ Sources/Uberball.Game.Services.MatchService.ConsoleHost/Program.cs
@@ -1,8 +1,15 @@
 ﻿
 namespace Uberball.Game.Services.MatchService.ConsoleHost {
+	using System;
+	using System.Net;
 
 	class Program {
 		static void Main(string[] args) {
+			MatchService service = new MatchService();
+			service.Start(new IPEndPoint(IPAddress.Any, 4530));
+
+			Console.WriteLine("Match service has been started. Press any key to quit.");
+			Console.ReadKey();
 		}
 	}
 }
---
+++ Sources/Uberball.Game.Services.MatchService/MatchService.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Uberball.Game.Services.MatchService {
+	using Khrussk.NetworkRealm;
+	using System.Net;
+
+	public class MatchService {
+		public MatchService() {
+			_service = new RealmService();
+		}
+
+		public void Start(IPEndPoint endpoint) {
+			_service.Start(endpoint);
+		}
+
+		public void Stop() {
+			_service.Stop();
+		}
+
+		RealmService _service;
+	}
+}
