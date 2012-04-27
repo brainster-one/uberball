3d3cc2b449d54982f232822a86526752d33689b0
brainster-one <brainster-one@hotmail.com>
2012-04-27 21:44:01 +0700

    Build script has been updated

--- build.cmd
+++ build.cmd
@@ -1,1 +1,11 @@
+@echo off
 Assets\MSBuild.exe .\Sources\Uberball.sln
+
+
+set "exclude=.log .dll .gitkeep .html .exe .xap clientaccesspolicy.xml" 
+for /f "tokens=*" %%i in ( 
+'dir /b binaries ^| findstr /l /v "%exclude%"' 
+) do ( 
+	del /q binaries\%%i
+	rd /q binaries\%%i
+) 
